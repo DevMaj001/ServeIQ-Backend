d9e5dbeeb21b3f7ba92c7c90250f987ab6274742
DennisMaj <charlesgift231@gmail.com>
2026-07-15 16:12:47 +0100

    fix: add AuditService + AuditLog to OrderModule providers

--- apps/api/src/modules/order/order.module.ts
+++ apps/api/src/modules/order/order.module.ts
@@ -7,12 +7,14 @@ import { Order } from './entities/order.entity';
 import { MenuItem } from '../menu/entities/menu-item.entity';
 import { Tab } from '../tab/entities/tab.entity';
 import { Department } from '../department/entities/department.entity';
+import { AuditLog } from '../../entities/audit-log.entity';
+import { AuditService } from '../../common/services/audit.service';
 import { IngredientModule } from '../ingredient/ingredient.module';
 import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, MenuItem, Tab, Department]), IngredientModule, NotificationModule],
-  providers: [OrderService, OrderScheduler],
+  imports: [TypeOrmModule.forFeature([Order, MenuItem, Tab, Department, AuditLog]), IngredientModule, NotificationModule],
+  providers: [OrderService, OrderScheduler, AuditService],
   controllers: [OrderController],
   exports: [OrderService],
 })
